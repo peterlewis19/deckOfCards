53a3bed3d0415b79ceb49e7180293a6cffb398eb
peterlewis19 <19lewis_p@bexleygs.co.uk>
2024-10-16 14:14:03 +0100

    Fixed outOfBounds errors, getHandValue, NOW IT FULLY WORKS

--- src/Pontoon.java
+++ src/Pontoon.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pontoon {
@@ -14,20 +16,19 @@ public class Pontoon {
     }
 
     public void run(int players){
-        Card[] houseCards = new Card[11];
-
-        Card[] userCards = new Card[11];
+        ArrayList<Card> houseCards = new ArrayList<>();
+        ArrayList<Card> playerCards = new ArrayList<>();
 
         //gives initial cards to player and house
-        userCards[0] =  hit();
-        houseCards[0] = hit();
-        userCards[1] = hit();
-        houseCards[1] = hit();
+        for (int i = 0; i< 2; i++) {
+            playerCards.add(hit());
+            houseCards.add(hit());
+        }
 
-        System.out.println("You have: "+ userCards[0] + ", " + userCards[1]);
-        System.out.println("The house has "+ houseCards[0] + ",  (" + houseCards[1] + ")");
+        System.out.println("You have: "+ playerCards.get(0) + ", " + playerCards.get(1));
+        System.out.println("The house has "+ houseCards.get(0) + ",  (" + houseCards.get(1) + ")");
 
-        while (getHandValue(userCards) <= 21 && getHandValue(houseCards) <= 21){
+        while (getHandValue(playerCards) < 21 && getHandValue(houseCards) < 21){
             //asks user if they wish to hit or stand
             System.out.println("Hit or Stand? (h or s)");
             String choice = getInput.nextLine();
@@ -36,19 +37,17 @@ public class Pontoon {
             turnCount++;
 
             if (choice.equals("h")){ //player hits
-                userCards[turnCount] = hit();
+                playerCards.add(hit());
                 //now the house
-                houseCards[turnCount] = hit();
+                houseCards.add(hit());
             } else{//player stands
                 standCount ++;
+                //PLAYER STANDS, house adds cards until its greater than value of playerCards
+                while (getHandValue(houseCards) <= getHandValue(playerCards)){
+                    houseCards.add(hit());
 
-                //int houseCardStanding = turnCount;
-                while (getHandValue(houseCards) <= 21 && getHandValue(houseCards) <= getHandValue(userCards)){
-                    houseCards[turnCount+1] = hit();
-
-                    //System.out.println("The house has: ");
                     for (int i = 0; i<=turnCount; i++){
-                        System.out.print(houseCards[i] +" ");
+                        System.out.print(houseCards.get(i) +" ");
                     }
                     System.out.println("Value of: " + getHandValue(houseCards));
 
@@ -58,23 +57,25 @@ public class Pontoon {
 
             //users Cards
             System.out.println("You have : ");
-            for (int i = 0; i<=turnCount; i++){
-                System.out.print(userCards[i] +" ");
+            for (int i = 0; i<playerCards.size(); i++){
+                System.out.print(playerCards.get(i) +" ");
             }
-            System.out.println("Value of: " + getHandValue(userCards));
+            System.out.println("Value of: " + getHandValue(playerCards));
 
             //the houses cards
 
             System.out.println("The house has: ");
-            for (int i = 0; i<=turnCount; i++){
-                System.out.print(houseCards[i] +" ");
+            System.out.print(houseCards.get(0));
+
+            for (int i = 1; i<houseCards.size(); i++){
+                System.out.print("(" + houseCards.get(i) +") ");
             }
             System.out.println("Value of: " + getHandValue(houseCards));
         }
 
-        if (getHandValue(houseCards) > 21 && getHandValue(userCards) < 21){
+        if (getHandValue(houseCards) > 21 && getHandValue(playerCards) < 21){
             System.out.println("User wins");
-        } else if (getHandValue(userCards) == 21 && getHandValue(houseCards) != 21){
+        } else if (getHandValue(playerCards) == 21 && getHandValue(houseCards) != 21){
             System.out.println("user wins");
         } else{
             System.out.println("The house wins");
@@ -89,23 +90,22 @@ public class Pontoon {
     }
 
     //gets the value of a hand
-    public int getHandValue(Card[] hand){
+    public int getHandValue(ArrayList<Card> hand){
         int handValue = 0;
 
         //adds each cards value to find the hand value
-        for (int i=0; i<=turnCount; i++){
-            if (hand[i].getNumberValue() > 10) {
-                handValue = handValue+ 10;
-            } else if(hand[i].getNumberValue() == 1){
+        for (Card card : hand) {
+            if (card.getNumberValue() > 10) {
+                handValue = handValue + 10;
+            } else if (card.getNumberValue() == 1) {
                 handValue = handValue + 11;
-                while (handValue >= 21){
-                    handValue = handValue-10;
+                while (handValue > 21) {
+                    handValue = handValue - 10;
                 }
-            } else{
-                handValue = handValue + hand[i].getNumberValue();
+            } else {
+                handValue = handValue + card.getNumberValue();
             }
         }
-
         return handValue;
     }
 }
