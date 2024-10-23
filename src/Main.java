d574a7ffd47174ee6f794bc600049c54605bf745
peterlewis19 <19lewis_p@bexleygs.co.uk>
2024-10-23 15:08:03 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        BlackJack newGame = new BlackJack();
 
-        //Card testCard = new Card(1, "Hearts");
-        //Deck thisDeck = new Deck();
-
-        //thisDeck.addsToDeck();
-        //thisDeck.displayDeck();
-        //thisDeck.shuffle();
-        //thisDeck.displayDeck();
-
-        //thisDeck.deal(7, 5);
-
-        Pontoon newGame = new Pontoon();
-
-        for (int i  = 0; i < 10; i++) {
-            newGame.run(1);
-        }
+        newGame.run(1);
     }
 }
